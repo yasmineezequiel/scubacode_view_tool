bd1692db645f83323c26ae7d56db65ecab4733bc
yasmineezequiel <yasmine.ezequiel@gmail.com>
2020-03-26 20:41:50 +0100

    Scubacode tool initial commit.

---
+++ lib/scubacode_view_tool.rb
@@ -0,0 +1,8 @@
+require "scubacode_view_tool/version"
+require "scubacode_view_tool/render"
+
+
+module ScubacodeViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/scubacode_view_tool/version.rb
@@ -0,0 +1,3 @@
+module ScubacodeViewTool
+  VERSION = "0.1.0"
+end
